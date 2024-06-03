867c622b7d99746ab16fcd63f44753fcfd15b95b
Angel Prat <aprat04@gmail.com>
2024-06-03 19:51:55 +0200

    fix speedometer title

--- src/Components/Charts/Speedometer.jsx
+++ src/Components/Charts/Speedometer.jsx
@@ -4,26 +4,28 @@ import ReactSpeedometer from 'react-d3-speedometer';
 function Speedometer(props) {
   
   return (
-    <ReactSpeedometer
-      width={250}
-      height={200}
-      needleHeightRatio={0.7}
-      minValue={0}
-      maxValue={100}
-      value={Math.round(props.value)}
-      currentValueText={
-        props.text && props.value !== undefined
-          ? props.text + ' ( ' + props.value.toString().slice(0, 4) + ' %)'
-          : 'Undefined'
-      }
-      customSegmentStops={props.data ? props.data.values.map(val => Math.round(val*100)) : []}
-      segmentColors={props.data ? props.data.colors : []}
-      ringWidth={47}
-      needleTransitionDuration={2222}
-      needleTransition="easeElastic"
-      needleColor={'#90f2ff'}
-      textColor={'#000'}
-    />
+    <div>
+      <ReactSpeedometer
+        width={250}
+        height={150}
+        needleHeightRatio={0.7}
+        minValue={0}
+        maxValue={100}
+        value={Math.round(props.value)}
+        customSegmentStops={props.data ? props.data.values.map(val => Math.round(val*100)) : []}
+        segmentColors={props.data ? props.data.colors : []}
+        ringWidth={47}
+        needleTransitionDuration={2222}
+        needleTransition="easeElastic"
+        needleColor={'#90f2ff'}
+        textColor={'#000'}
+      />
+      <div>
+        {props.text && props.value !== undefined
+            ? props.text + ' ( ' + props.value.toString().slice(0, 4) + ' %)'
+            : 'Undefined'}
+      </div>
+    </div>
   );
 }
 
